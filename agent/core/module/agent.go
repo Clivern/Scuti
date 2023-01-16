ac8f98934a127c365416570f89ab6bc7314503f3
Clivern <hello@clivern.com>
2023-01-16 15:22:41 +0100

    update the agent

---
+++ agent/core/module/agent.go
@@ -0,0 +1,28 @@
+// Copyright 2023 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package module
+
+type Agent struct {
+}
+
+// NewAgent creates a new instance
+func NewAgent() *Agent {
+	return &Agent{}
+}
+
+// Join join the host group under the management server
+func (a *Agent) Join() {
+
+}
+
+// Heartbeat send a heartbeat to management server
+func (a *Agent) Heartbeat() {
+
+}
+
+// Report report local actions to the management server
+func (a *Agent) Report() {
+
+}
